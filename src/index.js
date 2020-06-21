213223bb27d3cb978b30414a652bf976035d1ffa
Manoel Vieira <manoelv8@gmail.com>
2020-06-20 22:32:21 -0300

    Projeto da RocketSeat no GoStack com React Native

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+
+import api from './services/api';
+
+
+export default function App() {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        const response = await api.post('projects', {
+            title: `Novo Project ${Date.now()}`,
+            owner: 'Manoel Vieira' 
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project])
+    }
+
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={({ item: project }) => (
+                        <Text style={styles.project}>{project.title}</Text>
+                    )}
+                />
+
+                <TouchableOpacity onPress={handleAddProject} activeOpacity={0.6} style={styles.button}>
+                    <Text style={styles.buttonText}>Adicionar projeto</Text>
+                </TouchableOpacity>        
+
+            </SafeAreaView>
+            {/* <ScrollView style={styles.container}>
+                {projects.map(project => (
+                    <Text style={styles.project} key={project.id}>{project.title}</Text>
+                ))}
+            </ScrollView> */}
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1'  
+    },
+    projects: {
+        color: '#FFF', 
+        fontSize: 30,
+    },
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        boderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
